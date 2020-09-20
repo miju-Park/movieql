36b36d4af523751a555dde2937997ab1de9f0a29
mjpark <gi6238@naver.com>
2020-09-20 11:06:12 +0900

    Wrapping REST API

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,12 +1,11 @@
-import { addMovie, deleteMovie, getById, getMovies } from './db';
+import { getMovies } from './db';
 const resolvers = {
 	Query: {
-		movies: () => getMovies(),
-		movie: (_, { id }) => getById(id)
-	},
-	Mutation: {
-		addMovie: (_, { name, score }) => addMovie(name, score),
-		deleteMovie: (_, { id }) => deleteMovie(id)
+		movies: (_, { limit, rating }) => getMovies(limit, rating)
 	}
+	// Mutation: {
+	// 	addMovie: (_, { name, score }) => addMovie(name, score),
+	// 	deleteMovie: (_, { id }) => deleteMovie(id)
+	// }
 };
 export default resolvers;
